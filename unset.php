83925c23f30c43632d19a20615b1b560395666d2
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2024-09-29 14:10:41 +0800

    Delete unset.php

--- unset.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(isset($_POST['btn_Clear'])) {
-        session_start(); // Establish connection to the current session
-        session_unset(); // Delete all session variables
-        header('Location: index.php'); // Go back to homepage
-    };
-?>
